126b04aa9ad1723c0e4a1d9a0283f97908c6564a
Lucas <lulubinou51@gmail.com>
2024-10-29 12:02:54 +0100

    Finished frontend
    Just finished fronted part for the authentication ! Currently, when Register and Login buttons are clicked, they print fields values into the server console.

---
+++ app/auth/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex justify-center items-center bg-slate-100">{children}</div>
+  );
+};
+
+export default AuthLayout;
